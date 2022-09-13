d35f77c3b2bb16c0ca2cdc2bdc68dcd5e0f9b31d
Brian Cain <bcain@hashicorp.com>
2022-09-13 14:18:26 -0700

    Add switch for spinning on a given axis

--- Assets/Scripts/SpinObject.cs
+++ Assets/Scripts/SpinObject.cs
@@ -6,6 +6,9 @@ public class SpinObject : MonoBehaviour {
   public int rotationSpeed = 50;
   public bool disableSpin = false;
 
+  public bool spinY = true;
+  public bool spinZ = false;
+
   // Start is called before the first frame update
   void Start() {
   }
@@ -13,7 +16,11 @@ public class SpinObject : MonoBehaviour {
   // Update is called once per frame
   void Update() {
     if (!disableSpin) {
-      transform.Rotate (0,rotationSpeed*Time.deltaTime, 0);
+      if (spinY) {
+        transform.Rotate (0,rotationSpeed*Time.deltaTime, 0);
+      } else if (spinZ) {
+        transform.Rotate (0,0,rotationSpeed*Time.deltaTime);
+      }
     }
   }
 }
