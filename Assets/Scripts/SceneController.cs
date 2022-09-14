a89f37a559947773a706e9f3acd5292182e83b49
Brian Cain <bcain@hashicorp.com>
2022-09-14 10:53:44 -0700

    Rearrange controller code

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -45,6 +45,9 @@ public class SceneController : MonoBehaviour {
     [SerializeField]
     private AudioClip highscoreClip;
 
+    // Internal
+    private bool deployAnim = true;
+
     // Start is called before the first frame update
     void Start() {
       audioSource = GetComponent<AudioSource>();
@@ -57,53 +60,58 @@ public class SceneController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
       if (Input.GetKeyDown(KeyCode.Space)) {
-        Debug.Log("Space was pressed!");
-        // Use this as a linear bool state machine to trigger animations
-        if (!showPipe) {
-          pipe.SetActive(true);
-          showPipe = true;
-          showWaypoint = true;
-
-          Debug.Log("Starting pipe animation...");
-          Animator anim = pipe.GetComponent<Animator>();
-
-          anim.SetTrigger("Rise");
-          audioSource.PlayOneShot(pipeUp, 1f);
-        } else if (showWaypoint) {
-          waypoint.SetActive(true);
-          showWaypoint = false;
-          showStar = true;
-
-          Debug.Log("Starting waypoint animation...");
-          audioSource.PlayOneShot(oneUpClip, 1f);
-        } else if (showStar) {
-          waypoint.SetActive(false);
-          star.SetActive(true);
-          audioSource.PlayOneShot(powerMeterRefillClip, 1f);
-
-          showQuestionBlock = true;
-          showStar = false;
-
-          Debug.Log("Starting Star animation...");
-        } else if (showQuestionBlock) {
-          Animator anim = star.GetComponent<Animator>();
-          anim.SetTrigger("Lower");
-          audioSource.PlayOneShot(pipeDown, 1f);
-
-          //star.SetActive(false);
-          questionBlock.SetActive(true);
-          Animator blockAnim = questionBlock.GetComponent<Animator>();
-          StartCoroutine(Utilities.DoAfter(1.5f, ()=>blockAnim.SetTrigger("Rise")));
-          StartCoroutine(Utilities.DoAfter(2.25f, ()=>audioSource.PlayOneShot(coinClip, 1f)));
-
-          showQuestionBlock = false;
-          Debug.Log("Starting Question Block animation...");
-          deployBlock = true;
-        } else if (deployBlock) {
-          Animator anim = questionBlock.GetComponent<Animator>();
-          anim.SetTrigger("Deploy");
-          audioSource.PlayOneShot(pipeDown, 1f);
+        if (deployAnim) {
+          Deploy();
         }
+      }
+    }
+
+    void Deploy() {
+      // The big linear state machine
+      if (!showPipe) {
+        pipe.SetActive(true);
+        showPipe = true;
+        showWaypoint = true;
+
+        Debug.Log("Starting pipe animation...");
+        Animator anim = pipe.GetComponent<Animator>();
+
+        anim.SetTrigger("Rise");
+        audioSource.PlayOneShot(pipeUp, 1f);
+      } else if (showWaypoint) {
+        waypoint.SetActive(true);
+        showWaypoint = false;
+        showStar = true;
+
+        Debug.Log("Starting waypoint animation...");
+        audioSource.PlayOneShot(oneUpClip, 1f);
+      } else if (showStar) {
+        waypoint.SetActive(false);
+        star.SetActive(true);
+        audioSource.PlayOneShot(powerMeterRefillClip, 1f);
+
+        showQuestionBlock = true;
+        showStar = false;
+
+        Debug.Log("Starting Star animation...");
+      } else if (showQuestionBlock) {
+        Animator anim = star.GetComponent<Animator>();
+        anim.SetTrigger("Lower");
+        audioSource.PlayOneShot(pipeDown, 1f);
+
+        //star.SetActive(false);
+        questionBlock.SetActive(true);
+        Animator blockAnim = questionBlock.GetComponent<Animator>();
+        StartCoroutine(Utilities.DoAfter(1.5f, ()=>blockAnim.SetTrigger("Rise")));
+        StartCoroutine(Utilities.DoAfter(2.25f, ()=>audioSource.PlayOneShot(coinClip, 1f)));
+
+        showQuestionBlock = false;
+        Debug.Log("Starting Question Block animation...");
+        deployBlock = true;
+      } else if (deployBlock) {
+        Animator anim = questionBlock.GetComponent<Animator>();
+        anim.SetTrigger("Deploy");
+        audioSource.PlayOneShot(pipeDown, 1f);
       }
     }
 }
